20a863fd320153a2b50e8b1858adc4ecdfca0cdb
elisaul batista <elisaulbatista@pursuit.org>
2023-12-09 19:00:37 -0500

    set up the queries

---
+++ queries/projects.js
@@ -0,0 +1,69 @@
+const db = require("../db/db.Config.js");
+
+const getAllProjects = async () => {
+  try {
+    const allProject = await db.any("SELECT * FROM projects");
+    return allProject;
+  } catch (err) {
+    return err;
+  }
+};
+
+const getOneproject = async (id) => {
+  try {
+    const oneProject = await db.one("SELECT * FROM projects WHERE id=$1", id);
+    return oneProject;
+  } catch (error) {
+    return error;
+  }
+};
+
+const createProject = async (project) => {
+  try {
+    const createdProject = await db.one(
+      "INSERT INTO projects (  title, descriptions, repolink, deployedlink) VALUES($1,$2,$3,$4) RETURNING *",
+      [
+        project.title,
+        project.descriptions,
+        project.repolink,
+        project.deployedlink,
+      ]
+    );
+    return createdProject;
+  } catch (error) {
+    return error;
+  }
+};
+
+const deleteProject = async (id) => {
+  try {
+    const deletedProject = await db.one(
+      "DELET FROM project WHERE id=$1 RETURNING *",
+      id
+    );
+    return deletedProject;
+  } catch (error) {
+    return error;
+  }
+};
+
+const updateProject = async (project) => {
+  try {
+    const { title, descriptions, repolink, deployedlink } = project;
+    const updatedProject = await db.one(
+      "UPDATE project SET title=$1, descriptions=$2,repolink=$3,deployedlink=$4 WHERE id=$5 RETURNING *",
+      [title, descriptions, repolink, deployedlink]
+    );
+    return updatedProject;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = {
+  getAllProjects,
+  getOneproject,
+  updateProject,
+  deleteProject,
+  createProject,
+};
