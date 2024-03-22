e7e8273ca83d0103461c76372f0641f4ac1eb192
elisaul batista <elisaulbatista@pursuit.org>
2024-03-22 19:49:20 -0400

    added controller and setup app.js fopr user

---
+++ Controllers/userControllers.js
@@ -0,0 +1,70 @@
+const express = require("express");
+
+const {
+  getAllUser,
+  getOneUser,
+  createUser,
+  deleteUser,
+  updateUser,
+} = require("../queries/user.js");
+
+const users = express.Router();
+
+users.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const oneUser = await getOneUser(id);
+  if (oneUser) {
+    res.json(oneUser);
+  } else {
+    res.status(404).json({ error: "User Not Found" });
+  }
+});
+
+users.get("/", async (req, res) => {
+  console.log("GET Request received for all users.");
+  const allUsers = await getAllUsers();
+  if (allUsers[0]) {
+    res.status(200).json({ success: true, data: { payload: allUsers } });
+  } else {
+    res.status(404).json({ success: false, data: { error: "No users found" } });
+  }
+});
+
+users.post("/", async (req, res) => {
+  try {
+    const createdUser = await createUser(req.body);
+    res.status(201).json(createdUser);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ success: false, error: "Cannot create user" });
+  }
+});
+
+users.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(`Delete request for user with id ${id} received`);
+    const deletedUser = await deleteUser(id);
+    if (deletedUser) {
+      res.status(200).json({ success: true, payload: { data: deletedUser } });
+    } else {
+      res.status(404).json({ success: false, error: "User Not Found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+users.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(`PUT request for user with id ${id} received.`);
+  const updatedUser = await updateUser(id, req.body);
+  if (updatedUser.id) {
+    res.status(200).json(updatedUser);
+  } else {
+    res.status(404).json({ success: false, error: "User Not Found" });
+  }
+});
+
+module.exports = users;
--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const projectsControllers = require("./Controllers/projectsControllers.js");
+const userControllers = require("./Controllers/userControllers.js");
+
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/projects", projectsControllers);
+app.use("/users", userControllers);
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to Batista Portfolio /projects to see data");
